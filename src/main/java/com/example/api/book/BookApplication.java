5ab0c1af7f502c789ca5cbcdd24d624b3ecf1779
swlee9087 <sw_lee9087@naver.com>
2021-08-13 17:14:10 +0900

    13 aug 1714

--- src/main/java/com/example/api/book/BookApplication.java
+++ src/main/java/com/example/api/book/BookApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication @ComponentScan
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class,
 		HibernateJpaAutoConfiguration.class})
